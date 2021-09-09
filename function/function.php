8948ed77f2ceb96938e9e06ed523b170dfdf5b04
Fauvel Louka <louka.fauvel@gmail.com>
2021-09-09 21:58:32 +0200

    feat : les fonctions et les pages

---
+++ function/function.php
@@ -0,0 +1,35 @@
+<?php
+function getNav(){
+  echo "
+  <nav class='bg-dark'>
+    <div class='container'>
+      <ul class='nav'>
+        <li class='nav-item'>
+          <a class='nav-link link-danger' href='index.php'>Accueil</a>
+        </li>
+
+        <li class='nav-item'>
+          <a class='nav-link link-danger' href='index.php'>À propos</a>
+        </li>
+
+        <li class='nav-item'>
+          <a class='nav-link link-danger' href='index.php'>Compétence</a>
+        </li>
+
+        <li class='nav-item'>
+          <a class='nav-link link-danger' href='index.php'>Experience</a>
+        </li>
+
+        <li class='nav-item'>
+          <a class='nav-link link-danger' href='index.php'>Formation</a>
+        </li>
+
+        <li class='nav-item'>
+          <a class='nav-link link-danger' href='index.php'>Contact</a>
+        </li>
+      </ul>
+    </div>
+  </nav>";
+}
+
+?>
---
+++ page/A_propos.php
@@ -0,0 +1,10 @@
+<div class="container">
+  <p class="text-start mx-5" style="width: 50%;">
+    Je suis élève en deuxième année de BTS Service informatique aux organisations
+    au pôle SupAvenir Sainte Ursule de Caen en Normandie.
+    J'aime beaucoup voyager et découvrir de nouvelles traditions culturelles.
+    J'ai eu la chance de participer à deux échanges Erasmus avec la Suède et l'Espagne en 2019.
+    Je fais du dessin à l'Esam de Caen et je pratique le théâtre depuis l'âge de 7 ans.
+    Je suis un élève sérieux qui aime travailler en équipe sur de nouveaux projets.
+  </p>
+</div>
