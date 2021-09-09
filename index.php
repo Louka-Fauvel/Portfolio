6a0071d84abb0504b8bcf761917a1ab62378503e
Fauvel Louka <louka.fauvel@gmail.com>
2021-09-09 12:03:57 +0200

    feat : index.php

--- index.php
+++ index.php
@@ -2,9 +2,37 @@
 <html>
   <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <title>Page d'accueil</title>
   </head>
   <body>
-    <p>Hello world !</p>
+
+    <nav class="bg-dark">
+      <div class="container">
+        <ul class="nav">
+          <li class="nav-item">
+            <a class="nav-link link-danger" href="index.php">Accueil</a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link link-danger" href="index.php">À propos</a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link link-danger" href="index.php">Compétence</a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link link-danger" href="index.php">Experience</a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link link-danger" href="index.php">Formation</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
   </body>
 </html>
